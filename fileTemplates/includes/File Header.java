1d3fea949c39ccf46e6dd8cf702ed1ddd699e977
0 <903274768@qq.com>
2020-01-07 13:11:59 +0800

    IU-2019.3.1 <0@calories Create project.default.xml, laf.xml, userDefine.xml, jdk.table.xml, Default.xml, path.macros.xml, ui.lnf.xml, File Header.java, wsl.distributions.xml, filetypes.xml, colors.scheme.xml, _@user_Darcula.icls, editor.xml, templates.xml, Ladies Night 2.icls, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,10 @@
+/**
+  *
+  * @ProjectName: ${PROJECT_NAME}
+  * @ClassName: ${NAME}
+  * @Description: TODO(一句话描述该类的功能)
+  * @Author: ${USER}
+  * @Date: ${DATE} ${TIME}
+  * @version v1.0
+  *
+  */
